20640240f24a82e3448759808abbe8cc6a73b170
Stephen Ridings <SRidings777@aol.com>
2018-09-04 00:58:29 -0500

    fgh
    fgh

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,7 +1,5 @@
-//Dependencies
-var path = require('path');
 
-//ROUTING
+var path = require('path');
 
 module.exports = function(app){
   //default GET route that leads to home.html - displays home page
@@ -9,7 +7,6 @@ module.exports = function(app){
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  //a USE route to home page
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
