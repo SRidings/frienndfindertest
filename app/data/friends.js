0e7903c0377967df369c65c03458304a82f42da0
Stephen Ridings <SRidings777@aol.com>
2018-09-03 22:47:43 -0500

    cdc
    csdc

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,171 +1,168 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-var friends = [
-  {
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+ {
       "name":"Owen",
       "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
       "scores":[
-         5,
-         1,
-         4,
-         4,
-         5,
-         1,
-         2,
-         5,
-         4,
-         1
+         "5",
+         "1",
+         "4",
+         "4",
+         "5",
+         "1",
+         "2",
+         "5",
+         "4",
+         "1"
           ]
       },
   {
   "name":"Pavan",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
+     "5",
+     "1",
+     "4",
+     "4",
+     "5",
+     "1",
+     "2",
+     "5",
+     "4",
+     "1"
       ]
 },
 {
   "name":"Angel",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     9,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
+     "9",
+     "1",
+     "4",
+     "4",
+     "5",
+     "1",
+     "2",
+     "5",
+     "4",
+     "1"
       ]
 },
 {
   "name":"Karina",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Miriam",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Lisa",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Nicole",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Jason",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Nate",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 },
 {
   "name":"Ken",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
-     5,
-     1,
-     4,
-     3,
-     2,
-     6,
-     9,
-     8,
-     9,
-     9
+     "5",
+     "1",
+     "4",
+     "3",
+     "2",
+     "6",
+     "9",
+     "8",
+     "9",
+     "9"
       ]
 }
 ]
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friends;
+
+module.exports = friendList;
