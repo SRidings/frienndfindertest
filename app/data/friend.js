d2cc59456a5384863468ea2d2546a80aef22ac1b
Stephen Ridings <SRidings777@aol.com>
2018-09-03 23:25:27 -0500

    sdf
    sdf

--- app/data/friend.js
+++ app/data/friend.js
@@ -35,7 +35,7 @@ var friendList = [
 },
 {
   "name":"Angel",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"https://i.imgur.com/Xf0HLPo.jpg",
   "scores":[
      "9",
      "1",
