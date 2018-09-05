e577a93035378496cdc5a12a230a342acf776201
Stephen Ridings <SRidings777@aol.com>
2018-09-04 20:49:28 -0500

    fsdf
    sdfsd

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+// Pull in required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Configure the Express application
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-app.listen(PORT, function () {
-  console.log('App listening on PORT: ' + PORT);
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
